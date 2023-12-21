e562f3a3b51b0ba062e9319530d35295eff1f62e
TheSecurityLead <sgrie10@wgu.edu>
2023-12-21 03:49:48 +0300

    Final

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ class Forecast {
   }
 }
 
+// Movie Class Definition
+class Movie {
+  constructor(data) {
+    this.title = data.title;
+    this.overview = data.overview;
+    this.average_votes = data.vote_average;
+    this.total_votes = data.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    this.popularity = data.popularity;
+    this.released_on = data.release_date;
+  }
+}
+
 function handleError(res, error) {
     console.error(error); // Log the error for server-side debugging
     res.status(500).send({ error: 'Something went wrong.' });
@@ -42,7 +55,21 @@ app.get('/weather', async (req, res) => {
     handleError(error, res);
   }
 });
-  
+
+// Route for Movies
+app.get('/movies', async (req, res) => {
+  const city = req.query.city;
+  const apiKey = process.env.MOVIE_API_KEY;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${city}`;
+
+  try {
+    const response = await axios.get(url);
+    const movies = response.data.results.map(movie => new Movie(movie));
+    res.send(movies);
+  } catch (error) {
+    res.status(500).send('Error fetching movie data');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
